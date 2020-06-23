64d3be08e06865674a1692c57fb8df3b9d53954e
bdsqqq <igorbedesqui@gmail.com>
2020-06-23 16:15:09 -0300

    implemented project creation, updated Project type

--- src/Components/Folio/index.tsx
+++ src/Components/Folio/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { FiArrowRight } from 'react-icons/fi'
 
 import db from '../../firebase';
 
@@ -12,16 +13,41 @@ const mq = breakpoints.map(
     bp => `@media (min-width: ${bp}em)`
 )
 
-const Wrapper = styled.div`
-    margin: -15px 2rem;
+const FormWrap = styled.div`
+    width: 30%;
+    margin: 8rem 4rem 4rem;
+    height: 10vh;
 `
-const Title = styled.span`
-    font-size: 3em;
-    color: #f3f3f3;
+const Form = styled.form`
+    display: flex;
+    width: 100%;
+`
+const InputNewId = styled.input`
+    width: 100%;
+    border: none;
+    border-bottom: 2px solid #bebed2;
+    padding: 5px;
+    height: 1.8em;
+    outline: none;
+    font-size: 2em;
+    transition: 200ms ease-in all;
 
-    ${mq[0]}{
-        font-size: 4em;
+    
+    &:focus{
+        border-bottom: 2px solid #78788c;
     }
+`
+const ButtonCreate = styled.button`
+    cursor: pointer;
+
+    width: 1.8em;
+    height: 1.8em;
+    text-align: center;
+    color: #00ad7c;
+    font-size: 2em;
+    border: none;
+
+    background-color: #00000000;
 `
 const CardContainer = styled.div`
     width: 100%;
@@ -49,7 +75,7 @@ const CardContainer = styled.div`
 
 interface Project{
     id: string;
-    ano: number;
+    ano: string;
     demo: string;
     img: string;
     projeto: string;
@@ -61,6 +87,7 @@ interface Project{
 function Folio() {
 
     const [isLoaded, setIsloaded] = useState(false);
+    const [newId, setNewId] = useState<string>('')
     const [projects, setProjects] = useState<Project[]>([]) //creates a state variable called projects and a method to update it
 
     useEffect(() => {
@@ -76,13 +103,44 @@ function Folio() {
 
     console.log(projects, "array");
 
+    const onChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+        setNewId(event.target.value);
+    }
+
+    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if(newId === '') return;
+        db.ref(`projects/${newId}`).set({
+            id: newId,
+            ano: "",
+            demo: "",
+            img: "",
+            projeto: "",
+            role: "",
+            source: "",
+            tools: []
+        })
+        .then(res => {
+            console.log('then')
+        })
+        .catch(err => console.log(err));
+    }
+
     return (
         <>
-            <Wrapper>
-                <Title>
-                    Folio
-            </Title>
-            </Wrapper>
+            <FormWrap>
+                <Form onSubmit={onSubmit}>
+                    <InputNewId 
+                            type="text"
+                            name="newId"
+                            placeholder="Cadastre um Projeto..."
+                            value={newId}
+                            onChange={onChange}
+                    />
+                    <ButtonCreate type="submit"><FiArrowRight style={{height:"1em", top: ".125em",position: "relative"}} /></ButtonCreate>
+                </Form>
+            </FormWrap>
+            
 
             <CardContainer>
                 {isLoaded === false ? <> <Loader />  <Loader />  <Loader /></> :
