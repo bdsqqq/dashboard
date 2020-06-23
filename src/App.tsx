aecd687f0c3939dca41110292a8cdad3e01f6534
bdsqqq <igorbedesqui@gmail.com>
2020-06-23 12:02:23 -0300

    updated styles for footer implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components'
 
 import { AuthProvider } from './Auth'
 import Routes from './routes'
 
+const Wrapper = styled.div`
+  min-height: 100vh;
+`
 function App() {
-  return <AuthProvider><Routes /></AuthProvider>
+  return  (
+    <Wrapper>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </Wrapper>
+  );
 }
 
 export default App;
