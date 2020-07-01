0e2e5cb5f2f88c60c5a4255fb825d4d6decef671
bdsqqq <igorbedesqui@gmail.com>
2020-07-01 18:35:57 -0300

    created useObjIsEmpty custom hook

---
+++ src/hooks/useObjIsEmpty.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const objIsEmpty = (obj:object) => {
+    for(let key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
+const useObjIsEmpty = () => {
+    return objIsEmpty
+  }
+  
+  export default useObjIsEmpty;
