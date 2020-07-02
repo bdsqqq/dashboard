ffa77f6a7eafa6cfdf9db5cb7cc924a909f3a6ac
bdsqqq <igorbedesqui@gmail.com>
2020-07-02 17:29:13 -0300

    removed styled components declaration from render method

--- src/Components/FolioCard/index.tsx
+++ src/Components/FolioCard/index.tsx
@@ -2,7 +2,68 @@ import React from 'react';
 import styled from 'styled-components';
 import { FiEdit3, FiInfo } from "react-icons/fi";
 
-interface FolioCardProps{
+const MetaWrapper = styled.div`
+    position: absolute;
+    right: -5px;
+    bottom: -15px;
+    width: 95%;
+    max-height: 105%;
+     overflow: auto;
+
+     &::-webkit-scrollbar{
+        width: 0.25rem;
+        height: 0.25rem;
+    }
+
+    &::-webkit-scrollbar-track{
+        background: #000000;
+    }
+
+    &::-webkit-scrollbar-thumb{
+        background: #f3f3f3;
+    }
+`
+
+const CardWrapper = styled.div`
+    position: relative;
+    margin: 20px auto;
+    height: fit-content;
+    width: 95%;
+`
+const CardImage = styled.img`
+    width: 95%;
+    display: block;
+    height: auto;
+    border-radius: 3px;
+    object-fit: cover;
+    transition: all 200ms cubic-bezier(0.785, 0.010, 0.000, 1.415);
+`
+
+const Meta = styled.div`
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+
+    border-radius: 3px;
+    padding: 15px 15px 10px;
+    width: 95%;        
+    background: rgba(0, 0, 0, 0.9);
+
+    color: #f2f2f2;
+    font-size: 1.6rem;
+
+    overflow: hidden;
+`
+const IconContainer = styled.div`        
+    > * {
+        cursor: pointer;
+
+        margin: 0 5px;
+    }
+`
+const MetaName = styled.div``
+
+interface FolioCardProps {
     project: any;
 
     setIsEditable: any;
@@ -10,79 +71,12 @@ interface FolioCardProps{
     setCurrentProject: any;
 }
 
-function Card(props:FolioCardProps){
+function Card(props: FolioCardProps) {
 
-    const breakpoints = [48, 64]
 
-    const mq = breakpoints.map(
-        bp => `@media (min-width: ${bp}em)`
-    )
-
-    const MetaWrapper = styled.div`
-        position: absolute;
-        right: -5px;
-        bottom: -15px;
-        width: 95%;
-        max-height: 105%;
-        overflow: auto;
-
-        &::-webkit-scrollbar{
-            width: 0.25rem;
-            height: 0.25rem;
-        }
-
-        &::-webkit-scrollbar-track{
-            background: #000000;
-        }
-
-        &::-webkit-scrollbar-thumb{
-            background: #f3f3f3;
-        }
-    `
-
-    const CardWrapper = styled.div`
-        position: relative;
-        margin: 20px auto;
-        height: fit-content;
-        width: 95%;
-    `
-    const CardImage = styled.img`
-        width: 95%;
-        display: block;
-        height: auto;
-        border-radius: 3px;
-        object-fit: cover;
-        transition: all 200ms cubic-bezier(0.785, 0.010, 0.000, 1.415);
-    `
-    
-    const Meta = styled.div`
-        display: flex;
-        align-items: flex-start;
-        justify-content: space-between;
-
-        border-radius: 3px;
-        padding: 15px 15px 10px;
-        width: 95%;        
-        background: rgba(0, 0, 0, 0.9);
-
-        color: #f2f2f2;
-        font-size: 1.6rem;
-
-        overflow: hidden;
-    `
-    const IconContainer = styled.div`        
-        > * {
-            cursor: pointer;
-
-            margin: 0 5px;
-        }
-    `
-    const MetaName = styled.div`
-    `
-
-    return(
+    return (
         <CardWrapper>
-            <CardImage src={props.project.img} alt={"Imagem do projeto "+props.project.projeto}/>
+            <CardImage src={props.project.img} alt={"Imagem do projeto " + props.project.projeto} />
             <MetaWrapper>
                 <Meta>
                     <MetaName>
@@ -93,7 +87,7 @@ function Card(props:FolioCardProps){
                             props.setCurrentProject(props.project)
                             props.setVisible(true);
                             props.setIsEditable(true);
-                        }} /> 
+                        }} />
                         <FiInfo onClick={() => {
                             props.setCurrentProject(props.project)
                             props.setVisible(true);
