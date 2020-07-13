2a461353c95cad86913f01bfa1831507d51986f1
bdsqqq <igorbedesqui@gmail.com>
2020-07-13 18:34:48 -0300

    updated styles to account for FolioDrawerForm removal

--- src/Components/FolioDrawerForm/index.tsx
+++ src/Components/FolioDrawerForm/index.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Form = styled.form`
-    max-width: 700px;
-    margin: 0 auto;
-`
+  width: 100%;
+  padding: 2em;
+`;
 
-const FolioDrawerForm = (props:any) => {
-    return (
-        <Form onSubmit={props.onSubmit}>
-            {props.children}
-        </Form>
-    );
-}
+const FolioDrawerForm = (props: any) => {
+  return <Form onSubmit={props.onSubmit}>{props.children}</Form>;
+};
 
-export default FolioDrawerForm
+export default FolioDrawerForm;
