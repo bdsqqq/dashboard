3d62cca43484f614d5771593a615ec2ede95881d
bdsqqq <igorbedesqui@gmail.com>
2020-07-14 17:07:43 -0300

    removed unused styled-components import

--- src/Components/Folio/index.tsx
+++ src/Components/Folio/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import { db } from "../../firebase";
 
 import CardContainer from "../FolioCardContainer";
