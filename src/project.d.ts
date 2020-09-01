e283d32fb818b22c80fcabfa6022e3f59a8f5d6c
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-01 03:54:06 -0300

    added project.d.ts

---
+++ src/project.d.ts
@@ -0,0 +1,13 @@
+
+export interface Project {
+    id: string;
+    year: string;
+    demo?: string;
+    img: string;
+    name: string;
+    role: string;
+    source?: string;
+    tools: string[];
+    order: string;
+    height?: any;
+}
